b23002ed83541c737e892d9efb9572b77c253cf1
Jordan Pritt <pritt.jordan@gmail.com>
2022-05-13 21:15:15 -0400

    Added more controls and screens.

--- src/main/java/com/wgu/softwareone/controllers/AddPartController.java
+++ src/main/java/com/wgu/softwareone/controllers/AddPartController.java
@@ -1,4 +1,44 @@
 package com.wgu.softwareone.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
 public class AddPartController {
+
+    @FXML
+    TextField partName;
+    @FXML
+    TextField partInventory;
+    @FXML
+    TextField partCost;
+    @FXML
+    TextField partMin;
+    @FXML
+    TextField partMax;
+    @FXML
+    TextField partMachineOrCompanyName;
+
+    @FXML
+    Button savePartButton;
+    @FXML
+    Button cancelPartButton;
+
+    @FXML
+    private void changeMachineOrCompanyName(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void saveNewPart(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void cancelAddPart(ActionEvent event) {
+        Stage stage = (Stage)cancelPartButton.getScene().getWindow();
+        stage.close();
+    }
 }
--- src/main/java/com/wgu/softwareone/controllers/AddProductController.java
+++ src/main/java/com/wgu/softwareone/controllers/AddProductController.java
@@ -1,4 +1,44 @@
 package com.wgu.softwareone.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
 public class AddProductController {
+
+    @FXML
+    TextField productName;
+    @FXML
+    TextField productInventory;
+    @FXML
+    TextField productCost;
+    @FXML
+    TextField productMin;
+    @FXML
+    TextField productMax;
+    @FXML
+    TextField productMachineOrCompanyName;
+
+    @FXML
+    Button saveProductButton;
+    @FXML
+    Button cancelProductButton;
+
+    @FXML
+    private void changeMachineOrCompanyName(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void saveNewProduct(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void cancelAddProduct(ActionEvent event) {
+        Stage stage = (Stage)cancelProductButton.getScene().getWindow();
+        stage.close();
+    }
 }
